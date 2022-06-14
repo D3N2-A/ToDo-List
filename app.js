130aab325a8c7bf294562ea3992a47e6b00046cc
D3N2-A <anmolsharma999937@gmail.com>
2022-06-14 15:00:40 +0530

    add list functionality

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+var items = [];
 const app = express();
-
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  var day = today.getDay();
-  var dayName = days[day];
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var dayInfo = today.toLocaleDateString("us-en", options);
+  res.render("list", { dayName: dayInfo, newTask: items });
+});
 
-  // if (day === 6 || day === 0) {
-  // } else {
-  // }
-  res.render("list", { nameOfDay: dayName });
+app.post("/", function (req, res) {
+  var item = req.body.task;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function () {
