277f8c13997b912e201b1e7f3e28639fb879b6dd
D3N2-A <anmolsharma999937@gmail.com>
2022-06-16 08:01:29 +0530

    module.exports

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const getDate = require("./date.js");
+const date = require(__dirname + "/date.js");
 let items = ["Complete lecture", "Goto Jim", "Light weight baby"];
 const app = express();
 app.set("view engine", "ejs");
@@ -8,13 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let dayInfo = today.toLocaleDateString("us-en", options);
+  let dayInfo = getDate();
   res.render("list", { dayName: dayInfo, newTask: items });
 });
 
