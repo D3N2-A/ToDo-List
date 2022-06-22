ae522ee767a08cc4e9e633acfcb987abf4f31ae5
D3N2-A <anmolsharma999937@gmail.com>
2022-06-22 07:59:11 +0530

    mongoose setup

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const getDate = require("./date.js");
 const date = require(__dirname + "/date.js");
-let items = ["Complete lecture", "Goto Jim", "Light weight baby"];
+// let items = ["Complete lecture", "Goto Jim", "Light weight baby"];
+const mongoose = require("mongoose");
 const app = express();
+mongoose.connect("mongodb://localhost.com:27017/td");
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const itemSchema = new mongoose.schema({
+  content: String,
+});
+const item = mongoose.model("item", itemSchema);
 app.get("/", function (req, res) {
   let dayInfo = getDate();
   res.render("list", { dayName: dayInfo, newTask: items });
